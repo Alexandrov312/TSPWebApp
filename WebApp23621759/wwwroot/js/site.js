ea13d63b37e43a7a53854004c1dd4463848f605a
Alexandrov312 <jassen.alexandrov@gmail.com>
2026-04-06 23:14:11 +0300

    Delete Task -> PopUp Menu
    При опит за изтриване на Task излиза до бутона PopUp меню, питайки потребителя за потвърждение.

--- WebApp23621759/wwwroot/js/site.js
+++ WebApp23621759/wwwroot/js/site.js
@@ -2,7 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//При зареждане на страницата се изпълнява тази функция
 document.addEventListener("DOMContentLoaded", function () {
+    //Намира всички елементи в страницата, които имат клас .toast-notification
     const toasts = document.querySelectorAll(".toast-notification");
 
     toasts.forEach((toast, index) => {
@@ -14,4 +17,31 @@ document.addEventListener("DOMContentLoaded", function () {
             setTimeout(() => toast.remove(), 300);
         }, 3000 + index * 300);
     });
+});
+
+//Слуша за всеки клик
+document.addEventListener("click", function (event) {
+
+    // показване на popup
+    if (event.target.closest(".show-popup")) {
+        event.preventDefault();
+
+        //Премахва всички вече отворени popup прозорци
+        document.querySelectorAll(".delete-popup")
+            .forEach(p => p.classList.remove("show"));
+
+        const wrapper = event.target.closest(".delete-wrapper");
+        wrapper.querySelector(".delete-popup").classList.add("show");
+    }
+
+    // cancel бутон
+    if (event.target.closest(".cancel-btn")) {
+        event.target.closest(".delete-popup").classList.remove("show");
+    }
+
+    // клик извън popup
+    if (!event.target.closest(".delete-wrapper")) {
+        document.querySelectorAll(".delete-popup")
+            .forEach(p => p.classList.remove("show"));
+    }
 });
